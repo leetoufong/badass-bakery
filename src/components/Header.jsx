defb1c428b5e115d572b7d58a59246f762d3e110
Tou Fong Lee <leetoufong@gmail.com>
2025-04-15 00:02:11 -0400

    Add conditional styling for header title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
+import { useLocation } from "react-router";
+
 const Header = (props) => {
     const { title } = props;
+    const location = useLocation();
+
+    console.log(location.pathname)
 
     return (
-        <header className="billboard bg-pink-light h-[85vh] flex items-center justify-center">
-            <h1 className="font-display uppercase text-8xl text-pink-800">{title}</h1>
+        <header className={location.pathname === '/' ? 'billboard bg-pink-light h-[85vh] flex items-center justify-center' : ''}>
+            <h1 className={`font-display uppercase  text-pink-800 ${location.pathname === '/' ? 'text-8xl' : 'text-6xl'}`}>{title}</h1>
         </header>
     )
 }
