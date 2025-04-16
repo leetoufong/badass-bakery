bb0f3fa657b67bf50d4670b4703430773644c7d6
Tou Fong Lee <leetoufong@gmail.com>
2025-04-15 22:40:51 -0400

    Making USD conversion helper function an import. Adding it throughout.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,18 +1,13 @@
 import { useState } from "react";
 import { Link } from "react-router";
+import { formatUSD } from "../helpers/formatUSD";
 
 const Cart = (props) => {
     const { cart } = props;
     const [ isActive, setIsActive ] = useState(false);
 
-    const formatUSDHandler = new Intl.NumberFormat('en-us', {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: 2
-    });
-
     return (
-        <div className="fixed top-0 right-0 p-4 bg-white">
+        <div className="shadow-sm fixed top-0 right-0 p-3 bg-white z-100">
             <p>{ cart.length > 0 && `${cart.length} Item${cart.length === 1 ? '' : 's'}` }</p>
 
             {isActive && (
@@ -20,16 +15,16 @@ const Cart = (props) => {
                     {cart.map((item, index) => {
                         return (
                             <li key={index}>
-                                {item.title} {formatUSDHandler.format(item.price)}
+                                {item.title} {formatUSD.format(item.price)}
                             </li>
                         )
                     })}
                 </ul>
             )}
             
-            <p><strong>Sub Total: {formatUSDHandler.format(cart.reduce((accumulator, currentValue) => accumulator + currentValue.price, 0))}</strong></p>
+            <p><strong>Sub Total: {formatUSD.format(cart.reduce((accumulator, currentValue) => accumulator + currentValue.price, 0))}</strong></p>
 
-            <Link className="btn mt-4" to="/checkout">Check out</Link>
+            <Link className="btn mt-2 inline-block" to="/checkout">Check out</Link>
         </div>
     )
 }
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,19 @@
-const Checkout = () => {
+import { formatUSD } from "../helpers/formatUSD";
+
+const Checkout = (props) => {
+    const { cart } = props;
+    
     return (
-        <div>Checkout</div>
+        <ul>
+            {cart.map((item, index) => {
+                return (
+                    <li key={index}>
+                        {item.title} {formatUSD.format(item.price)}
+                    </li>
+                )
+            })}
+        </ul>
     )
 }
 
-export default Checkout
+export default Checkout;
