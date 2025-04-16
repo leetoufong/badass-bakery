5c5a8ef3fdb4fcda6a08a7961b23c9e1540914ae
Tou Fong Lee <leetoufong@gmail.com>
2025-04-15 22:40:16 -0400

    More tailwindy css. Better product image UI.

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,17 +1,21 @@
+import { formatUSD } from "../helpers/formatUSD";
+
 const Products = (props) => {
     const { setCart, data, items } = props;
 
     return (
-        <ul className="grid grid-cols-4 gap-4">
+        <ul className="grid sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-8">
             {data.products.map((item, index) => {
                 return (
                     <li key={index}>
-                        <img src={item.image} />
-                        <h4 className="text-lg font-bold">{item.title}</h4>
+                        <div className="h-0 relative overflow-hidden pb-[75%] rounded-[0_14px_0_14px]">
+                            <img className="w-full absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]" src={item.image} />
+                        </div>
+                        <p className="text-lg font-bold">{item.title}</p>
                         <p>{item.description}</p>
-                        <p>{item.price}</p>
+                        <p>{formatUSD.format(item.price)}</p>
                         <button
-                            className="btn"
+                            className="btn mt-4"
                             onClick={() => {
                                 setCart(prev => [...prev, item]);
                             }}
