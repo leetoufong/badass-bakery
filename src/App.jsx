1b9dcb902d0f2966a6fab7ed200e94d34832a6b8
Tou Fong Lee <leetoufong@gmail.com>
2025-04-15 22:39:23 -0400

    Add another product and better pictures. Passing cart info to Checkout page.

--- src/App.jsx
+++ src/App.jsx
@@ -18,36 +18,43 @@ const App = () => {
                     "id": "423D-3G2D-DGX12",
                     "title": "Hello Sundae",
                     "descripton": "The best hello world you'll ever taste",
-                    "image": "https://fpoimg.com/600x400",
+                    "image": "https://thefirstyearblog.com/wp-content/uploads/2023/05/Hot-Fudge-Sundae-8.jpg",
                     "price": 4.50
-                },
-                {
-                    "id": "M35H-ADFQ-LB91",
-                    "title": "Sponge Cake",
-                    "descripton": "Topped inside and out with strawberries, blue berries, kiwi and tangerine.",
-                    "image": "https://fpoimg.com/600x400",
-                    "price": 29.99
                 },
                 {
                     "id": "C003-B123-449Z",
                     "title": "Steamed Coconut Layered Cake",
                     "descripton": "Coconut milk steamed into creamy goodness. ",
-                    "image": "https://fpoimg.com/600x400",
+                    "image": "https://www.saveur.com/uploads/2019/02/08/J4362HZVIUZBUTFLJKCGPKLB5U.jpg",
                     "price": 12.00
+                },
+                {
+                    "id": "M35H-ADFQ-LB91",
+                    "title": "Sponge Cake",
+                    "descripton": "Topped inside and out with strawberries, blue berries, kiwi and tangerine.",
+                    "image": "https://www.errenskitchen.com/wp-content/uploads/2018/06/Vanilla-Sponge-Cake-1-recipe-card.jpg",
+                    "price": 29.99
                 },
                 {
                     "id": "0D2D-BAAS-COF3",
                     "title": "Vietnamese Iced Coffee",
                     "descripton": "Able to cool you down and pucker you up all at the same time. Made with condensed milk.",
-                    "image": "https://fpoimg.com/600x400",
+                    "image": "https://boulderphoco.com/wp-content/uploads/2020/04/Vietnamese-cafe-with-condensed-milk-600x400.jpg",
                     "price": 3.99
                 },
                 {
                     "id": "BA3Z-39FF-ADG4",
                     "title": "Sesame Balls",
                     "descripton": "Friend to perfection and filled with your choice of soybean paste or taro.",
-                    "image": "https://fpoimg.com/600x400",
+                    "image": "https://i0.wp.com/vocabularyoffood.com/wp-content/uploads/2023/07/Air-Fried-Sesame-Balls-1.jpg",
                     "price": 10.25
+                },
+                {
+                    "id": "BD0F-MVLW-193I",
+                    "title": "Tri-Color Dessert",
+                    "descripton": "Friend to perfection and filled with your choice of soybean paste or taro.",
+                    "image": "https://i0.wp.com/chawjcreations.com/wp-content/uploads/2019/01/IMG_7531.jpg",
+                    "price": 5.15
                 }
             ]
         }
@@ -57,7 +64,7 @@ const App = () => {
             "id": "423D-3G2D-DGX12",
             "title": "Hello Sundae",
             "descripton": "The best hello world you'll ever taste",
-            "image": "https://fpoimg.com/600x400",
+            "image": "https://thefirstyearblog.com/wp-content/uploads/2023/05/Hot-Fudge-Sundae-8.jpg",
             "price": 4.50
         }
     ]);
@@ -66,7 +73,7 @@ const App = () => {
         <Routes>
             <Route path="/" element={ <Layout data={data} cart={cart} /> }>
                 <Route index element={ <Products setCart={setCart} data={data} /> } />
-                <Route path="/checkout" element={ <Checkout /> } />
+                <Route path="/checkout" element={ <Checkout cart={cart} /> } />
             </Route>
         </Routes>
     )
