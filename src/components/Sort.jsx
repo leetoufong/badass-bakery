d1c989cb452620752d37442be3512c3041475a6b
Tou Fong Lee <leetoufong@gmail.com>
2025-04-23 17:10:24 -0400

    Fix issue where sorting was adding a NaN product to the render

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -2,21 +2,19 @@ const Sort = (props) => {
     const { data, setProducts } = props;
 
     const handleProductSort = (value) => {
-        if (value.trim().length) {
-            switch (value) {
-                case 'price-low':
-                    setProducts(prevProducts => [...prevProducts.sort((a, b) => a.price - b.price)]);
-                    break;
-                case 'price-high':
-                    setProducts(prevProducts => [...prevProducts.sort((a, b) => b.price - a.price)]);
-                    break;
-                // case 'rating-low':
-                //     break;
-                // case 'rating-low':
-                //     break;
-                default:
-                    setProducts(data?.products);
-            }
+        switch (value) {
+            case 'price-low':
+                setProducts(prevProducts => [...prevProducts.sort((a, b) => a.price - b.price)]);
+                break;
+            case 'price-high':
+                setProducts(prevProducts => [...prevProducts.sort((a, b) => b.price - a.price)]);
+                break;
+            // case 'rating-low':
+            //     break;
+            // case 'rating-low':
+            //     break;
+            default:
+                setProducts(data.products);
         }
     }
 
@@ -24,7 +22,7 @@ const Sort = (props) => {
         <header className="flex justify-end mb-8">
             <label htmlFor="sort"><strong>Sort by</strong>:</label>
             <select id="sort" onChange={(event) => handleProductSort(event.target.value)}>
-                <option value="">All</option>
+                <option>All</option>
                 <option value="price-low">Lowest Price</option>
                 <option value="price-high">Highest Price</option>
                 {/* <option value="rating-low">Lowest Rating</option>
